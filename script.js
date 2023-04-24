c6b07262ae1bccbb9f6d6565b0eba056a1921de8
s <sharon.woo@outlook.com>
2023-04-24 14:53:18 +0800

    update view counter, update page layout, add icons for languages etc later

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://vryowpy57u7p7j7of27pp4penm0jvipw.lambda-url.ap-southeast-1.on.aws/");
+    let response = await fetch("https://gk7g4wdbrmfz4ejltqrcmaff6m0ztqja.lambda-url.ap-southeast-1.on.aws/");
     let views = await response.json();
     counter.innerHTML = `<p style='font-size:10px;'>👀 : ${views} views</p>`;
 
